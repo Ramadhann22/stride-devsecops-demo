014a8dcf96ac66a9215134a40d2509f262e4c5b9
Ramadhann22 <wildangalih678@gmail.com>
2026-06-29 04:30:02 +0000

    Mitigation: Enforce signed commits

--- app.py
+++ app.py
@@ -1129,12 +1129,5 @@ def dashboard_summary():
         traceback.print_exc()
         return jsonify({"error": str(e)}), 500
     
-@app.route("/debug")
-def debug():
-    return {
-        "status": "debug enabled.",
-        "message": "Development Debug Endpoint!"
-    }
-    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
